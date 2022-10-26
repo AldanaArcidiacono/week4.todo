fa1cf069f0f9d9e393f45124185cbbe7f5808a28
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-26 13:28:54 +0200

    Add menu

--- src/infrastructure/components/routes/app.routes.tsx
+++ src/infrastructure/components/routes/app.routes.tsx
@@ -1,12 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from '../../../share/home/page/home.page';
 import ToDoPage from '../../../share/todo/page/todo.page';
 
 export function AppRoutes() {
     return (
         <Routes>
+            <Route path="" element={<HomePage></HomePage>}></Route>
             <Route path="home" element={<HomePage></HomePage>}></Route>
             <Route path="todo" element={<ToDoPage></ToDoPage>}></Route>
+            <Route
+                path="*"
+                element={<Navigate replace to=""></Navigate>}
+            ></Route>
         </Routes>
     );
 }
