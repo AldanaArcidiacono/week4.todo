cba867270531554c65c28ec8793ca56d3f13e9fc
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-26 15:41:56 +0200

    Starting to learn testing

---
+++ src/infrastructure/components/header/header.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Header } from './header';
+import { MemoryRouter as Router } from 'react-router-dom';
+
+test('renders learn react link', () => {
+    render(
+        <Router>
+            <Header />
+        </Router>
+    );
+    const element = screen.getByText(/react/i); // Expresion regular
+    expect(element).toBeInTheDocument();
+});
